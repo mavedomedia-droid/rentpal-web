ffc0d1cc748689d57897a372217450ee0cf98f54
mavedomedia-droid <mavedomedia@gmail.com>
2026-09-09 13:21:07 +0100

    Update rentpal-config.js

--- rentpal-config.js
+++ rentpal-config.js
@@ -3,7 +3,7 @@
 
 var RENTPAL_CONFIG = {
   // Main RentPal backend (Users, Intro Requests, etc.)
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyspUyz-Lg_RaaT5bgxWKUrPNaSFsVJMSsCO8alRwJJZ86s7HUOFa6UnTSivcTTTsPR0Q/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbx7GD8JfjeRdeFd33GOZDjupMd6QWnzm9D7msYAuz-QqefrvYEdY1lRU2gqtaiDswD2OA/exec',
   // Other shared settings
   SITE_URL: 'https://rentpal-web.netlify.app',
   WHATSAPP: '+234 816 2373 553'
